54fd7be38b4311694c65d4b60b5fd730456806c7
jon-eikholm <joneikholm@hotmail.com>
2023-09-11 13:48:00 +0200

    Update Notebook_start.js
    starter kode til Firebase

--- Notebook_start.js
+++ Notebook_start.js
@@ -1,46 +1,20 @@
-import { app, database } from './firebase'
-import { collection, addDoc, deleteDoc, doc, updateDoc } from 'firebase/firestore';
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, FlatList, Button, View, TextInput, Text } from 'react-native';
-import { useCollection } from 'react-firebase-hooks/firestore'; //install with: $ npm install react-firebase-hooks
-
 
 export default function App() {
   const [text, setText] = useState('')
+  const [notes, setNotes] = useState([])
   const [editObj, setEditObj] = useState(null)
-  const [values, loading, error] = useCollection(collection(database, "notes"))
-  const data = values?.docs.map((doc) => ({...doc.data(), id: doc.id}))
+
   
-  async function buttonHandler(){
-    try{
-    await addDoc(collection(database, "notes"), {
-      text: text
-    })
-    }catch(err){
-      console.log("fejl i DB " + err)
-    }
-  }
-
-  async function deleteDocument(id){
-      await deleteDoc(doc(database, "notes", id))
-  }
-
-  function viewUpdateDialog(item){
-    // få noget at blive synligt
-    setEditObj(item)
-  }
-
-  async function saveUpdate(){
-      await updateDoc(doc(database, "notes", editObj.id), {
-        text: text
-      })
-      setText("")
-      setEditObj(null)
+ function buttonHandler(){
+    setNotes([...notes, {key:notes.length, name:text}])   
   }
 
   return (
     <View style={styles.container}>
+      <Text>Hello</Text>
       { editObj && 
         <View>
           <TextInput defaultValue={editObj.text} onChangeText={(txt) => setText(txt)} />
@@ -51,12 +25,10 @@ export default function App() {
       <TextInput style={styles.textInput}  onChangeText={(txt) => setText(txt)} />
       <Button title='Press Me' onPress={buttonHandler} ></Button>
       <FlatList
-        data={data}
+        data={notes}
         renderItem={(note) => 
           <View>
-            <Text>{note.item.text}</Text>
-            <Text onPress={() => deleteDocument(note.item.id)}>Delete</Text>
-            <Text onPress={() => viewUpdateDialog(note.item)}>Update</Text>
+            <Text>{note.item.name}</Text>
           </View>
       }
       />
