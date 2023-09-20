87be20651a80e03ffcc1e9b5824dedd9d4fa8e77
jon-eikholm <joneikholm@hotmail.com>
2023-09-20 15:06:50 +0200

    Create camera_start.js

---
+++ camera_start.js
@@ -0,0 +1,89 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, FlatList, Button, View, TextInput, Text, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker'
+import { storage} from './firebase'
+import { ref, uploadBytes, getDownloadURL} from 'firebase/storage'
+
+
+export default function App() {
+  const [text, setText] = useState('')
+  const [notes, setNotes] = useState([])
+  const [editObj, setEditObj] = useState(null)
+  const [imagePath, setImagePath] = useState(null)
+
+  
+ function buttonHandler(){
+    setNotes([...notes, {key:notes.length, name:text}])   
+  }
+
+  async function launchImagePicker(){
+      let result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true
+      })
+      if(!result.canceled){
+          setImagePath(result.assets[0].uri)
+      }
+  }
+
+  async function uploadImage(){
+    const res = await fetch(imagePath)
+    const blob = await res.blob()
+    const storageRef = ref(storage, "myimage.jpg")
+    uploadBytes(storageRef, blob).then((snapshot) => {
+      alert("image uploaded")
+    })
+  }
+
+  async function downloadImage(){
+    getDownloadURL(ref(storage, "myimage.jpg"))
+    .then((url) => {
+      setImagePath(url)
+    })
+    .catch((error) => {
+      alert("fejl i image download " + error)
+    })
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+      { editObj && 
+        <View>
+          <TextInput defaultValue={editObj.text} onChangeText={(txt) => setText(txt)} />
+          <Text onPress={saveUpdate}>Save</Text>
+        </View> 
+      }
+
+      <TextInput style={styles.textInput}  onChangeText={(txt) => setText(txt)} />
+      <Button title='Press Me' onPress={buttonHandler} ></Button>
+      <FlatList
+        data={notes}
+        renderItem={(note) => 
+          <View>
+            <Text>{note.item.name}</Text>
+          </View>
+      }
+      />
+      <Image style={{width:200, height:200}}  source={{uri:imagePath}} />
+      <Button title='Download image' onPress={downloadImage} />
+      <Button title='Pick image' onPress={launchImagePicker} />
+      <Button title='Upload image' onPress={uploadImage} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop:200
+  },
+  textInput:{
+    backgroundColor:'lightblue',
+    minWidth: 200
+  }
+});
