f8794331cdbc2b622cd801fcb43ca53d68016e31
jon-eikholm <joneikholm@hotmail.com>
2023-10-17 20:21:24 +0200

    added parameters

--- gpt_imports.py
+++ gpt_imports.py
@@ -4,3 +4,16 @@ import re
 import string
 import tensorflow as tf
 from tensorflow.keras import layers, models, losses, callbacks
+
+# parameters
+VOCAB_SIZE = 10000
+MAX_LEN = 80
+EMBEDDING_DIM = 256
+KEY_DIM = 256
+N_HEADS = 2
+FEED_FORWARD_DIM = 256
+VALIDATION_SPLIT = 0.2
+SEED = 42
+LOAD_MODEL = True
+BATCH_SIZE = 32
+EPOCHS = 1
