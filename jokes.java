b442460d88934077e2b663d11d4da34628ddf929
jon-eikholm <joneikholm@hotmail.com>
2024-01-29 15:34:43 +0100

    imports added

--- jokes.java
+++ jokes.java
@@ -1,3 +1,10 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+
 public void run(){
   try{ 
     HttpClient client = HttpClient.newHttpClient(); 
